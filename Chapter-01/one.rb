69777c42aa9b04357324748e90ae5ca5ccd8452f
sumanthkrishna <tosumanthkrishna@gmail.com>
2015-06-29 21:02:03 -0400

    Display methods
    Simple print, puts methods

---
+++ Chapter-01/one.rb
@@ -0,0 +1,11 @@
+# Ruby snippets to follow
+
+# Displays the text back to consoles
+print "Hello Ruby!"					
+
+#Hello Ruby!	=> nil
+
+puts "Hello Rails!"						# Displays the text back to conoles
+
+#Hello Rails!
+#=> nil
